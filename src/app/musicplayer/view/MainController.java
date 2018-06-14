dd8d91a04ca78a584b88f010759c1ef22e123fa5
lightning12569 <thangdtt2904@gmail.com>
2018-06-14 14:58:51 +0700

    Add search function

--- src/app/musicplayer/view/MainController.java
+++ src/app/musicplayer/view/MainController.java
@@ -261,6 +261,7 @@ public class MainController implements Initializable, IntellitypeListener {
     	}
     }
 
+    //Tạo popup tìm kiếm
 	private void createSearchPopup() {
 		try {
 
@@ -886,6 +887,7 @@ public class MainController implements Initializable, IntellitypeListener {
     	}
     }
 
+    //Hiển thị kết quả tìm kiếm
     public void showSearchResults(SearchResult result) {
         VBox root = (VBox) searchPopup.getScene().getRoot();
         ObservableList<Node> list = root.getChildren();
